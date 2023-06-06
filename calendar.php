e47e751d3648e3fbcd4be8ba68d584fd6fc4f78b
rail1dd <127716067+rail1dd@users.noreply.github.com>
2023-06-06 16:31:17 +0800

    calendar.html

--- calendar.php
+++ calendar.php
@@ -1,6 +1,16 @@
-<?php
-    $a = date("Y/md");
-    $txt = "http://img.owspace.com/Public/uploads/Download/$a.jpg";
-    header('Content-type: image/jpeg');
-    echo file_get_contents($txt);
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>日签图片</title>
+</head>
+<body>
+    <script>
+        var today = new Date();
+        var year = today.getFullYear();
+        var month = ("0" + (today.getMonth() + 1)).slice(-2);
+        var day = ("0" + today.getDate()).slice(-2);
+        var img_url = "http://img.owspace.com/Public/uploads/Download/" + year + "/" + month + day + ".jpg";
+        document.write("<img src=\"" + img_url + "\" alt=\"日签图片\">");
+    </script>
+</body>
+</html>
